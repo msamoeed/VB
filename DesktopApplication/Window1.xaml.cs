4fea5d7308590122c28836831f45a68d9e4f5b92
Muhammad Abdul Moeed <49859519+msamoeed@users.noreply.github.com>
2020-11-12 16:08:38 +0500

    new one
    first comit

---
+++ DesktopApplication/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopApplication
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string user = "msamoeed";
+        string pass = "password";
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Window2 signup = new Window2();
+            signup.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (this.username.Text.ToString() == user && this.password.Password.ToString() == pass)
+            {
+                MessageBox.Show("Logged In");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Username or Password");
+            }
+        }
+    }
+}
---
+++ DesktopApplication/Window2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopApplication
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        string user;
+        string ema;
+        string pass;
+        string dob;
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            user = this.username.Text.ToString();
+            pass = this.password.Text.ToString();
+            ema = this.email.Text.ToString();
+            dob = this.email.Text.ToString();
+            MessageBox.Show("Users registered with Email " + ema);
+        }
+    }
+}
